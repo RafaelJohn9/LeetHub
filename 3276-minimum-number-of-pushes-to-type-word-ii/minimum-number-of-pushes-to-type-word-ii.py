c1d4729a3ee5b4f25204b39eb375f5303724c275
JohnKagunda <125447154+RafaelJohn9@users.noreply.github.com>
2024-08-06 10:37:17 +0300

    Time: 94 ms (99.76%) | Memory: 17.5 MB (73.03%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,37 +1,11 @@
-from collections import Counter
-
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        """
-        Finds the minimum pushes to enter a word in a phone's keypad
-        """
-        CONST_KEYS = '1*#0'
-        NUM_OF_KEYS = 8
-        KEYPAD = {}
-
-        # Step 1: Sort word by frequency in descending order
-        count = Counter(word)
-        sorted_word = ''.join(sorted(word, key=lambda x: count[x], reverse=True))
-
-        # Helper function to check if a character is a special character
-        def is_special(char: str) -> bool:
-            return char in CONST_KEYS
-
-        min_pushes = 0
-        uniq_index = 0
-
-        for char in sorted_word:
-            if char in KEYPAD:
-                min_pushes += KEYPAD[char]
-                continue
-
-            # Assign the number of pushes based on the unique index and special character check
-            if is_special(char) or uniq_index < NUM_OF_KEYS:
-                KEYPAD[char] = 1
-            else:
-                KEYPAD[char] = (uniq_index // NUM_OF_KEYS) + 1
-
-            min_pushes += KEYPAD[char]
-            uniq_index += 1
-
-        return min_pushes
+        l=[0]*(32)
+        for i in range(26):
+            l[i]=word.count(chr(97+i))
+        l.sort(reverse=True)
+        res=0
+        for i in range(4):
+            for j in range(8):
+                res+=(i+1)*l[8*i+j]
+        return res
