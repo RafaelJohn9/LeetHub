e873cba9e680189fbc0e72309e29a23985e85ba0
JohnKagunda <125447154+RafaelJohn9@users.noreply.github.com>
2025-03-12 09:22:18 +0300

    0696-COUNT-BINARY-SUBSTRINGS Stats: Time: 69 ms (71.87%), Space: 18.1 MB (44.67%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.py
+++ 0696-count-binary-substrings/0696-count-binary-substrings.py
@@ -1,9 +1,9 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
-        prev_group = 0  # Count of previous group (0s or 1s)
-        curr_group = 1  # Count of current group (at least 1 character is there)
-        count = 0       # Result count of valid substrings
-        
+        prev_group = 0
+        curr_group = 1
+        count = 0
+
         for i in range(1, len(s)):
             if s[i] == s[i - 1]:
                 curr_group += 1
@@ -12,5 +12,6 @@ class Solution:
                 prev_group = curr_group
                 curr_group = 1
         
-        count += min(prev_group, curr_group)  # Add the last counted groups
+        count += min(prev_group, curr_group)
+
         return count
