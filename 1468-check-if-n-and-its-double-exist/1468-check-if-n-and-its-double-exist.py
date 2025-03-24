4d464bf2b6f14851eb40f55509793a29c34224c4
JohnKagunda <125447154+RafaelJohn9@users.noreply.github.com>
2025-03-24 10:54:35 +0300

    1468-CHECK-IF-N-AND-ITS-DOUBLE-EXIST Stats: Time: 4 ms (33.54%), Space: 17.7 MB (81.12%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,14 +1,14 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        arr.sort()
-        
+        biggest_num = max(arr)
         i = 0
         n = len(arr)
+        
         while i < n:
             if arr[i] == 0 and arr.count(0) == 1:
                 i += 1
                 
-            if arr[i] * 2 <= arr[-1] and  arr[i] * 2 in arr:
+            if arr[i] * 2 <= biggest_num and  arr[i] * 2 in arr:
                 return True
             i += 1
         
