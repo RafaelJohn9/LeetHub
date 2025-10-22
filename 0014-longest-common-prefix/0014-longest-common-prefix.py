6427f6e953191503a41bdf1a695ce48e9ca08cd0
JohnKagunda <125447154+RafaelJohn9@users.noreply.github.com>
2025-10-22 19:29:46 +0300

    0014-LONGEST-COMMON-PREFIX Stats: Time: 0 ms (100%), Space: 17.9 MB (48.08%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,43 +1,16 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # if the list has only one string we return the  string
-        if len(strs) == 1:
-            return strs[0]
+        if len(strs)==0 :
+            return ""
 
-        # Create a variable to hold the suggested prefix
-        common_prefix = ""
+        prefix=strs[0]
 
-        # Compare the first two strings
-        first_str = strs[0]
-        second_str = strs[1]
-        shortest_str_length = min(len(first_str), len(second_str))
+        for string in strs[1:]:
+            while not string.startswith(prefix):
+                prefix =prefix[:-1]
 
-        for i in range(shortest_str_length):
-            if first_str[i]  == second_str[i]:
-                common_prefix += first_str[i]
-            else:
-                break
-        
-        # if array contains only two elements we return prefix
-        if len(strs) == 2:
-            return common_prefix
-        
-        for i in range(2, len(strs)):
-            string = strs[i]
-            len_string = len(string)
+                if not prefix :
+                    return ""
 
-            for i in range(len(common_prefix)):
-                if len_string <= i:
-                    common_prefix = common_prefix[:i]
-                    break
-
-                if string[i]  == common_prefix[i]:
-                    continue
-                else:
-                    common_prefix = common_prefix[:i]
-                    break
-                
-        
-        return common_prefix
-
+        return prefix
 
