0ad26372f0f1d5b6a980b99f90643ee8aa5b065e
JohnKagunda <125447154+RafaelJohn9@users.noreply.github.com>
2026-04-02 05:53:31 +0300

    Time: 44 ms (45.54%), Space: 29.9 MB (91.07%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.rs
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.rs
@@ -1,54 +1,51 @@
-/** 
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
- use std::collections::HashMap;
- use rand::Rng;
+use std::collections::HashMap;
+use rand::Rng;
 
- struct RandomizedSet {
-    set: HashMap<i32, i32>,
- }
+struct RandomizedSet {
+    vals: Vec<i32>,
+    indices: HashMap<i32, usize>,
+}
 
 impl RandomizedSet {
 
     fn new() -> Self {
-        Self{
-            set: HashMap::new(),
+        Self {
+            vals: Vec::new(),
+            indices: HashMap::new(),
         }
     }
     
     fn insert(&mut self, val: i32) -> bool {
-        if (self.set.contains_key(&val)){
+        if self.indices.contains_key(&val) {
             return false;
         }
-        self.set.insert(val, val);
-        return true;
+
+        self.vals.push(val);
+        self.indices.insert(val, self.vals.len() - 1);
+        true
     }
     
     fn remove(&mut self, val: i32) -> bool {
-        if (self.set.contains_key(&val)){
-            self.set.remove(&val);
-            return true;
+        if let Some(&idx) = self.indices.get(&val) {
+            let last = *self.vals.last().unwrap();
+
+            // Move last element into idx
+            self.vals[idx] = last;
+            self.indices.insert(last, idx);
+
+            // Remove last element
+            self.vals.pop();
+            self.indices.remove(&val);
+
+            true
+        } else {
+            false
         }
-        return false
     }
     
     fn get_random(&self) -> i32 {
-        let map_len = self.set.len();
         let mut rng = rand::thread_rng();
-        let rand_num = rng.gen_range(0..map_len);
-
-        let map_keys: Vec<&i32> = self.set.keys().collect();
-        let key = map_keys[rand_num];
-
-        return *self.set.get(key).unwrap();
+        let idx = rng.gen_range(0..self.vals.len());
+        self.vals[idx]
     }
 }
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * let obj = RandomizedSet::new();
- * let ret_1: bool = obj.insert(val);
- * let ret_2: bool = obj.remove(val);
- * let ret_3: i32 = obj.get_random();
- */
